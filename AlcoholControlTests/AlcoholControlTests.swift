abf27b947c1eb44babffca0a50f5d38cab16fc59
Max Chesnikov <maxim.chesnikov@b2broker.com>
2026-02-11 15:08:11 +0300

    Initial Commit

---
+++ AlcoholControlTests/AlcoholControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlcoholControlTests.swift
+//  AlcoholControlTests
+//
+//  Created by Maxim Chesnikov on 11.02.2026.
+//
+
+import Testing
+@testable import AlcoholControl
+
+struct AlcoholControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
